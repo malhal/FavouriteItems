5e017ad4105d9b5ab9d19102544f824d28ad8878
Malcolm Hall <malhal@users.noreply.github.com>
2021-01-21 13:49:15 +0000

    Filenames from template updated

--- FavouriteItems/FavouriteItemsApp.swift
+++ FavouriteItems/FavouriteItemsApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct SwiftDataApp: App {
+struct FavouriteItemsApp: App {
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
--- FavouriteItems/Persistence.swift
+++ FavouriteItems/Persistence.swift
@@ -31,7 +31,7 @@ struct PersistenceController {
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "SwiftData")
+        container = NSPersistentContainer(name: "FavouriteItems")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
